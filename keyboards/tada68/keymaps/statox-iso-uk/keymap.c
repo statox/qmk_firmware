d8546ccc8813886c279723ceb1330d9cf97cfccc
Adrien Fabre <adrien.fabre@dashlane.com>
2019-05-21 22:50:18 +0200

    TADA68 - Add tap dance for accented a on function layout

--- keyboards/tada68/keymaps/statox-iso-uk/keymap.c
+++ keyboards/tada68/keymaps/statox-iso-uk/keymap.c
@@ -14,6 +14,7 @@
  * - Toggle LED with V
  * - Change LED brightness with c and b
  * - Tap danse on the E key to switch between é, è, ê and e
+ * - Tap danse on the A key to switch between à, â and a
  *
  * Base layout
  * - Use KC_LSPO and KC_RSPC (i.e shift when held, parenthesis when taped)
@@ -27,7 +28,8 @@
  * Tap Dance Declarations
  */
 enum {
-  E_AC
+    E_AC,
+    A_AC
 };
 
 /*
@@ -57,11 +59,31 @@ void accent_e (qk_tap_dance_state_t *state, void *user_data) {
     }
 }
 
+// Switch between à, â and a
+void accent_a (qk_tap_dance_state_t *state, void *user_data) {
+    switch (state->count) {
+        case 1:
+            unicode_input_start();
+            register_hex(0x00e0);   // à
+            unicode_input_finish();
+            break;
+        case 2:
+            unicode_input_start();
+            register_hex(0x00e2); // â
+            unicode_input_finish();
+            break;
+        default:
+            register_code(KC_A);
+            unregister_code(KC_A);
+    }
+}
+
 /*
  * Tap Dance Registrations
  */
 qk_tap_dance_action_t tap_dance_actions[] = {
-  [E_AC] = ACTION_TAP_DANCE_FN (accent_e)
+  [E_AC] = ACTION_TAP_DANCE_FN (accent_e),
+  [A_AC] = ACTION_TAP_DANCE_FN (accent_a)
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -90,7 +112,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------.
    * |   | F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12| Delete| Ins|
    * |----------------------------------------------------------------|
-   * |     |   | ↑ |Eac|   |   |   |Ms1|Ms2|   |   |   |   |     |Home|
+   * |     |Aac| ↑ |Eac|   |   |   |Ms1|Ms2|   |   |   |   |     |Home|
    * |-------------------------------------------------------    -----|
    * |       | ← | ↓ | → |   |   |MsL|MsD|MsU|MsL|   |  |   |    | End|
    * |----------------------------------------------------------------|
@@ -100,11 +122,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `----------------------------------------------------------------'
    */
   [_FL] = LAYOUT_iso(
-    _______, KC_F1,   KC_F2,   KC_F3,    KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_INS,  \
-    _______, _______, KC_UP,   TD(E_AC), _______, _______, _______, KC_BTN1, KC_BTN2, _______, _______, _______, _______,          KC_HOME, \
-    _______, KC_LEFT, KC_DOWN, KC_RGHT,  _______, _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______, _______, _______, KC_END,  \
-    _______, _______, _______, _______,  BL_DEC,  BL_TOGG, BL_INC,  _______, _______, _______, _______, _______, KC_MUTE, KC_VOLU, _______, \
-    _______, _______, _______,                    _______,                            _______, _______, _______, KC_BRID, KC_VOLD, KC_BRIU
+    _______, KC_F1,    KC_F2,   KC_F3,    KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_INS,  \
+    _______, TD(A_AC), KC_UP,   TD(E_AC), _______, _______, _______, KC_BTN1, KC_BTN2, _______, _______, _______, _______,          KC_HOME, \
+    _______, KC_LEFT,  KC_DOWN, KC_RGHT,  _______, _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______, _______, _______, KC_END,  \
+    _______, _______,  _______, _______,  BL_DEC,  BL_TOGG, BL_INC,  _______, _______, _______, _______, _______, KC_MUTE, KC_VOLU, _______, \
+    _______, _______,  _______,                    _______,                            _______, _______, _______, KC_BRID, KC_VOLD, KC_BRIU
 	),
 };
 
