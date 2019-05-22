5ed6732c6da89b0e63c5881ce66532f4687ffcbc
Adrien Fabre <adrien.fabre@dashlane.com>
2019-05-22 14:41:51 +0200

    TADA68 - Reformat rules.mk

--- keyboards/tada68/keymaps/statox-iso-uk/rules.mk
+++ keyboards/tada68/keymaps/statox-iso-uk/rules.mk
@@ -2,17 +2,20 @@
 #   change to "no" to disable the options, or define them in the Makefile in
 #   the appropriate keymap folder that will get included automatically
 #
-BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration(+1000)
+
+# Enabled
+TAP_DANCE_ENABLE = yes      # Allow tap dance (https://beta.docs.qmk.fm/features/feature_tap_dance)
 MOUSEKEY_ENABLE = yes       # Mouse keys(+4700)
 EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
-CONSOLE_ENABLE = no         # Console for debug(+400)
+UNICODE_ENABLE = yes        # Unicode
 COMMAND_ENABLE = yes        # Commands for debug and configuration
-NKRO_ENABLE = yes           # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
+NKRO_ENABLE = yes           # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+# Disabled
+BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration(+1000)
+CONSOLE_ENABLE = no         # Console for debug(+400)
 MIDI_ENABLE = no            # MIDI controls
 AUDIO_ENABLE = no           # Audio output on port C6
-UNICODE_ENABLE = yes        # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
-TAP_DANCE_ENABLE = yes      # Allow tap dance (https://beta.docs.qmk.fm/features/feature_tap_dance)
