18d93329d76d2029e4825ec2532c875cd1ed098e
Adrien Fabre <adrien.fabre@dashlane.com>
2019-05-23 12:22:11 +0200

    TADA68 - Add one shot symbols layer

--- keyboards/tada68/keymaps/statox-iso-uk/keymap.c
+++ keyboards/tada68/keymaps/statox-iso-uk/keymap.c
@@ -24,8 +24,9 @@
  * - Use CAPS to switch to the function layer like FN
  */
 
-#define _BL 0
-#define _FL 1
+#define _BL 0 // Base Layer
+#define _NL 1 // Navigation Layer
+#define _SL 2 // Symbols Layer
 
 /*
  * Tap Dance Declarations
@@ -89,6 +90,56 @@ qk_tap_dance_action_t tap_dance_actions[] = {
   [A_AC] = ACTION_TAP_DANCE_FN (accent_a)
 };
 
+
+enum custom_keycodes {
+  E_AIGUE = SAFE_RANGE,
+  C_CEDIL,
+  A_GRAVE,
+  U_GRAVE,
+  O_GRAVE,
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case A_GRAVE:
+            if (record->event.pressed) {
+                unicode_input_start();
+                register_hex(0x00e0);   // à
+                unicode_input_finish();
+            }
+            break;
+        case E_AIGUE:
+            if (record->event.pressed) {
+                unicode_input_start();
+                register_hex(0x00e9);   // é
+                unicode_input_finish();
+            }
+            break;
+        case O_GRAVE:
+            if (record->event.pressed) {
+                unicode_input_start();
+                register_hex(0x00f2);   // ò
+                unicode_input_finish();
+            }
+            break;
+        case U_GRAVE:
+            if (record->event.pressed) {
+                unicode_input_start();
+                register_hex(0x00f9);   // u grave
+                unicode_input_finish();
+            }
+            break;
+        case C_CEDIL:
+            if (record->event.pressed) {
+                unicode_input_start();
+                register_hex(0x00e7);   // ç
+                unicode_input_finish();
+            }
+            break;
+    }
+    return true;
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Keymap _BL: (Base Layer) Default Layer
    * ,----------------------------------------------------------------.
@@ -104,14 +155,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `----------------------------------------------------------------'
    */
   [_BL] = LAYOUT_iso(
-    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_GRV,  \
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,          KC_DEL,  \
-    MO(_FL), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,  KC_PGUP, \
-    KC_LSPO, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC, KC_UP,   KC_PGDN, \
-    KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                             KC_RALT, MO(_FL), KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
-	),
+    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS, KC_EQL,  KC_BSPC, KC_GRV,  \
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC, KC_RBRC,          KC_DEL,  \
+    MO(_NL), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT, KC_NUHS, KC_ENT,  KC_PGUP, \
+    KC_LSPO, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH, KC_RSPC, KC_UP,   KC_PGDN, \
+    KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                             KC_RALT, OSL(_SL), KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+    ),
 
-  /* Keymap _FL1: Function Layer 1
+  /* Keymap _NL: Navigation Layer 1
    * ,----------------------------------------------------------------.
    * |   | F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12| Delete|  1*|
    * |----------------------------------------------------------------|
@@ -130,20 +181,42 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *  - 3*  Toggle the Autoshift feature
    *  - 4*  Report the current Autoshift timeout
    */
-  [_FL] = LAYOUT_iso(
+  [_NL] = LAYOUT_iso(
     _______, KC_F1,    KC_F2,   KC_F3,    KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_ASUP, \
     _______, _______,  _______, TD(E_AC), _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, _______,          KC_ASDN, \
     _______, TD(A_AC), _______, _______,  KC_BTN1, KC_BTN2, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______, _______, _______, KC_ASTG, \
     _______, _______,  _______, _______,  BL_DEC,  BL_TOGG, BL_INC,  _______, _______, _______, _______, _______, KC_MUTE, KC_VOLU, KC_ASRP, \
     _______, _______,  _______,                    _______,                            _______, _______, _______, KC_BRID, KC_VOLD, KC_BRIU
-	),
+    ),
+
+  /* Keymap _SL: Symbols Layer 2
+   * ,----------------------------------------------------------------.
+   * |   | F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|       |    |
+   * |----------------------------------------------------------------|
+   * |     |   |   | é |   |   |   |   |   |   |   |   |   |     |    |
+   * |-------------------------------------------------------    -----|
+   * |       | à |   |   |   |   |   |   |   |   |   |  |   |    |    |
+   * |----------------------------------------------------------------|
+   * |     |   |   |   |   |   |   |   |   |    |   |  |      |  |    |
+   * |----------------------------------------------------------------|
+   * |    |    |    |                       |   |   |    |   |   |    |
+   * `----------------------------------------------------------------'
+   */
+
+  [_SL] = LAYOUT_iso(
+    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, \
+    _______, _______, _______, E_AIGUE, _______, _______, _______, U_GRAVE, _______, O_GRAVE, _______, _______, _______,          _______, \
+    _______, A_GRAVE, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, C_CEDIL, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______,                    _______,                           _______, _______, _______, _______, _______, _______
+    ),
 };
 
 /*
  * Set up the proper unicode mode for linux
  */
 void matrix_init_user(void) {
-	set_unicode_input_mode(UC_LNX); // or UC_WINC
+    set_unicode_input_mode(UC_LNX); // or UC_WINC
 };
 
   /* Keymap Template
@@ -158,4 +231,15 @@ void matrix_init_user(void) {
    * |----------------------------------------------------------------|
    * |    |    |    |                       |   |   |    |   |   |    |
    * `----------------------------------------------------------------'
+   */
+
+  /*
+   * Layout template
+   * [_XX] = LAYOUT_iso(
+   *   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+   *   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, \
+   *   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+   *   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+   *   _______, _______, _______,                    _______,                           _______, _______, _______, _______, _______, _______
+   *   ),
    */
