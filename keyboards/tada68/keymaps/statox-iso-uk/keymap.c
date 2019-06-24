d89f589fbe17f7d29085e5379144a5d41ab9e321
Adrien Fabre <adrien.fabre@dashlane.com>
2019-06-24 17:08:36 +0200

    TADA68: Add layout switch on ESC + Add application window switch in navigation layer

--- keyboards/tada68/keymaps/statox-iso-uk/keymap.c
+++ keyboards/tada68/keymaps/statox-iso-uk/keymap.c
@@ -29,6 +29,15 @@
 #define _SL 2 // Symbols Layer
 
 /*
+ * Custom keycodes
+ */
+// Keycodes to switch to an active application window
+// These keycodes are used in Ubuntu's shortcut manager and call a script using wnctrl
+#define SW_CHROM S(C(A(KC_C))) // Chromium
+#define SW_SLACK S(C(A(KC_S))) // Slack
+#define SW_TERMI S(C(A(KC_A))) // Current Terminal emulator
+
+/*
  * Tap Dance Declarations
  */
 enum {
@@ -147,7 +156,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Keymap _BL: (Base Layer) Default Layer
    * ,----------------------------------------------------------------.
-   * |Esc | 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp |~ ` |
+   * |FN3 | 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp |~ ` |
    * |----------------------------------------------------------------|
    * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|     |Del |
    * |-------------------------------------------------------    -----|
@@ -155,23 +164,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |----------------------------------------------------------------|
    * |Shi/(|  \ |  Z|  X|  C|  V|  B|  N|  M|  ,|  .| /|Rshi/)|Up|PgDn|
    * |----------------------------------------------------------------|
-   * |Ctrl|Win |Alt |  Layer _NL or Space   |Alt|FN2|Ctrl|Lef|Dow|Rig |
+   * |Ctrl|Win |Alt |          Space        |Alt|FN2|Ctrl|Lef|Dow|Rig |
    * `----------------------------------------------------------------'
    *
    * FN1 => Momentarily activate Navigation layer
    * FN2 => One shot symbol layer (TODO find something better to do with that)
+   * FN3 => If pressed alone send an escape if held with another key, trigger _SL
    */
   [_BL] = LAYOUT_iso(
-    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS, KC_EQL,  KC_BSPC, KC_GRV,  \
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC, KC_RBRC,          KC_DEL,  \
-    MO(_NL), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT, KC_NUHS, KC_ENT,  KC_PGUP, \
-    KC_LSPO, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH, KC_RSPC, KC_UP,   KC_PGDN, \
-    KC_LCTL, KC_LGUI, KC_LALT,                  LT(_SL, KC_SPC),                     KC_RALT, OSL(_SL), KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+    LT(_SL, KC_ESC),  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS, KC_EQL,  KC_BSPC, KC_GRV,  \
+    KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC, KC_RBRC,          KC_DEL,  \
+    MO(_NL),          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT, KC_NUHS, KC_ENT,  KC_PGUP, \
+    KC_LSPO,          KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH, KC_RSPC, KC_UP,   KC_PGDN, \
+    KC_LCTL,          KC_LGUI, KC_LALT,                      KC_SPC,                          KC_RALT, OSL(_SL), KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
   /* Keymap _NL: Navigation Layer 1
    * ,----------------------------------------------------------------.
-   * |   | F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12| Delete|  1*|
+   * |   |SP1|SP2|SP3| F4| F5| F6| F7| F8| F9|F10|F11|F12| Delete|  1*|
    * |----------------------------------------------------------------|
    * |     |   |   |Eac|   |   | ← | ↓ | ↑ | → |   |   |   |     |  2*|
    * |-------------------------------------------------------    -----|
@@ -194,13 +204,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * V+, V- => Change volume
    * B+, B- => Change screen brightness
    * Eac, Aac => Tap dances to get accentuated characters
+   * SP1 => Switch to the current active window of Chromium
+   * SP2 => Switch to the current active window of my current favorite terminal emulator
+   * SP3 => Switch to the current active window of Slack
    */
   [_NL] = LAYOUT_iso(
-    _______, KC_F1,    KC_F2,   KC_F3,    KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_ASUP, \
-    _______, _______,  _______, TD(E_AC), _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, _______,          KC_ASDN, \
-    _______, TD(A_AC), _______, _______,  KC_BTN1, KC_BTN2, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______, _______, _______, KC_ASTG, \
-    _______, _______,  _______, _______,  BL_DEC,  BL_TOGG, BL_INC,  KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, _______, KC_MUTE, KC_VOLU, KC_ASRP, \
-    _______, _______,  _______,                    _______,                            _______, _______, _______, KC_BRID, KC_VOLD, KC_BRIU
+    _______, SW_CHROM, SW_TERMI, SW_SLACK, KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_ASUP, \
+    _______, _______,  _______,  TD(E_AC), _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, _______,          KC_ASDN, \
+    _______, TD(A_AC), _______,  _______,  KC_BTN1, KC_BTN2, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______, _______, _______, KC_ASTG, \
+    _______, _______,  _______,  _______,  BL_DEC,  BL_TOGG, BL_INC,  KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, _______, KC_MUTE, KC_VOLU, KC_ASRP, \
+    _______, _______,  _______,                     _______,                            _______, _______, _______, KC_BRID, KC_VOLD, KC_BRIU
     ),
 
   /* Keymap _SL: Symbols Layer 2
